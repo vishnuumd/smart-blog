825902735c08595914308aacab3805b5c0ef060a
vishnu <vishnumdorisys.com>
2023-12-06 11:09:46 +0530

    first crud

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,80 @@
+@extends('products.layout')
+     
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Vishnu @ OrisysIndia</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+    
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success" id="success-message">
+            <p>{{ $message }}</p>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+        <style>
+            /* Define the CSS animations */
+            @keyframes slideUp {
+                from {
+                    opacity: 0;
+                    transform: translateY(100%);
+                }
+                to {
+                    opacity: 1;
+                    transform: translateY(0);
+                }
+            }
+    
+            /* Apply the animations to the success message */
+            .animated-slide-up {
+                animation: slideUp 0.5s ease-in-out;
+            }
+        </style>
+    
+        <script>
+            // Automatically hide the success message after 5 seconds (adjust as needed)
+            setTimeout(function () {
+                $('#success-message').fadeOut();
+            }, 5000);
+        </script>
+    @endif
+     
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Details</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td><img src="/image/{{ $product->image }}" width="100px"></td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->detail }}</td>
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+     
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+      
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+     
+                    @csrf
+                    @method('DELETE')
+        
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+    
+    {!! $products->links() !!}
+        
+@endsection
